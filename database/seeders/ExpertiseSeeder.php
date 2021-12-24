56cf6acec3761501fff69d8475fea5c7e893b176
hsf-apple <muhd.hasifx9@gmail.com>
2021-12-24 22:59:51 +0800

    expertise model migrate seeder

---
+++ database/seeders/ExpertiseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ExpertiseModel;
+
+class ExpertiseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
