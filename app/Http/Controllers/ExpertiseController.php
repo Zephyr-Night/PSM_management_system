9c65a35f1a514c6f436aebc41830cc2081887350
hsf-apple <muhd.hasifx9@gmail.com>
2021-12-24 23:27:47 +0800

    expertise model update

--- app/Http/Controllers/ExpertiseController.php
+++ app/Http/Controllers/ExpertiseController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ExpertiseModel;
 use Illuminate\Http\Request;
 
 class ExpertiseController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ExpertiseModel  $expertiseModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ExpertiseModel $expertiseModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ExpertiseModel  $expertiseModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ExpertiseModel $expertiseModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ExpertiseModel  $expertiseModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ExpertiseModel $expertiseModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ExpertiseModel  $expertiseModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ExpertiseModel $expertiseModel)
+    {
+        //
+    }
 }
