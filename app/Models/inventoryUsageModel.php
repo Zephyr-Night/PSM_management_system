bf3b234f14742bb40738a29adb964b5fc64eec8d
AymanAziz <89571593+AymanAziz@users.noreply.github.com>
2022-01-26 23:48:22 +0800

    budak

--- app/Models/inventoryUsageModel.php
+++ app/Models/inventoryUsageModel.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace app\Models;
 
+use App\Models\studentprofileModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
