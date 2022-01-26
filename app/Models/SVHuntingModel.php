9b5f5f64c728996678e1fd1338b8e8ccd19bb03e
Amirakamarul <96134994+Amirakamarul@users.noreply.github.com>
2022-01-26 20:43:38 +0800

    Update SVHuntingModel.php

--- app/Models/SVHuntingModel.php
+++ app/Models/SVHuntingModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
+use PDO;
 
 class SVHuntingModel extends Model
 {
