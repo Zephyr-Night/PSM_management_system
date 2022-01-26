4b4091f66b728e8f363da875ed1398542f8a1fb4
hsf-apple <muhd.hasifx9@gmail.com>
2022-01-26 23:57:01 +0800

    adminsidebar

--- resources/views/expertise/index.blade.php
+++ resources/views/expertise/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <link rel="stylesheet" href="/css/counting.css">
 @include('layouts.adminsidebar')
- <main class="py-4">  {{--create spacing --}}
+ <main class="py-4">  
     <div class="content">
         <div class="row justify-content-center">
             <div class="col-md-12">
--- resources/views/layouts/adminsidebar.blade.php
+++ resources/views/layouts/adminsidebar.blade.php
@@ -4,7 +4,9 @@
     <a href="{{route('LectureProfile.edit',Auth::user()->id  )}}">Profile</a>
     <a href="{{route('title.index')}}">Title Index</a>
     <a href="{{route('listRequestLecture')}}">Inventory Usage</a>
-    <a href="{{route('expertise.create')}}">Expertise Add</a>
+    <a href="{{route('ListLectureExpertise')}}">List Lecture</a>
+    <a href="{{route('expertise.index')}}">Expertise Edit/Add/Delete</a>
     <a href="{{route('Approval.index')}}">Approval List</a>
     <a href="{{route('indexlogbooklecture')}}">Logbook</a>
+
 </div>
