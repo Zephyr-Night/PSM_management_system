de20fff98a88c3f876968b565ff29e91ec5cbfe7
GitBeginner <nuraniss99@gmail.com>
2022-01-27 10:13:05 +0800

    Update LogbookController.php

--- app/Http/Controllers/LogbookController.php
+++ app/Http/Controllers/LogbookController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\LogbookModel;
 class LogbookController extends Controller
 {
-
+    //display the list of the logbook at student dashboard
     public function index()
     {
         $result = new LogbookModel();
@@ -21,7 +21,7 @@ class LogbookController extends Controller
       return view('logbook.index',compact(['listlogbookstudent','approve']));
     }
 
-
+    //create the new logbook and check the lecture(sv)
     public function create()
     {
         $result = new LogbookModel();
@@ -30,7 +30,7 @@ class LogbookController extends Controller
         return view('logbook.generatelogbook',compact(['checksv']));
     }
 
-
+    //store the new logbook data
     public function store(Request $request)
     {
         $result = new LogbookModel();
@@ -40,16 +40,17 @@ class LogbookController extends Controller
         return redirect('logbook');
     }
 
-
+    //display the logbook details and the approved supervisor of the specific id
     public function show($id)
     {
         $result = new LogbookModel();
-       $datauser = $result->showspecificlogbook($id);
-       $approve = $result->listlogbooktest();
+        $datauser = $result->showspecificlogbook($id);
+        $approve = $result->listlogbooktest();
 
-       return view('logbook.viewlogbookspecific',compact(['datauser','approve']));
+        return view('logbook.viewlogbookspecific',compact(['datauser','approve']));
     }
 
+    //edit the logbook of the specific id
     public function edit($id)
     {
         $result = new LogbookModel();
@@ -60,6 +61,7 @@ class LogbookController extends Controller
         return view('logbook.editlogbook',compact(['editlogbookdata','checksv']));
     }
 
+    //update the data of the specific requested id in the logbook list
     public function update(Request $request, $id)
     {
         $result = new LogbookModel();
@@ -83,6 +85,7 @@ class LogbookController extends Controller
         return redirect('logbook');
     }
 
+    //display the logbook list and the check the approved supervisor in lecture dashboard
     public function indexlogbooklecture()
     {
         $result = new LogbookModel();
@@ -93,6 +96,7 @@ class LogbookController extends Controller
         return view('logbook.logbookstudent',compact(['listlogbooklecture','checkapprovestudent']));
     }
 
+    //verify the logbook of thespecific id
     public function verifylogbook( $id)
     {
         $result = new LogbookModel();
@@ -104,13 +108,15 @@ class LogbookController extends Controller
         // print($editlogbookdata);
 
         return view('logbook.verifylogbook',compact(['editlogbookdata','checksv']));
-    }
+    }                                       
 
+    //confirm the verification logbook
     public function confirmationverifylogbook(Request $request, $id)
     {
         $result = new LogbookModel();
         $data = $request;
         $dataid = $id;
+        //update the data
         $result->PUTmethodlecture($data,$dataid);
 
         return redirect('indexlogbooklecture');
